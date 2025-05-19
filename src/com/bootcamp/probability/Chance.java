f74a3a7cbf85b9ee98bf5c10862a8d90a636363c
akshay-011 <102289107+akshay-011@users.noreply.github.com>
2025-05-19 13:49:28 +0530

    Adds equals to chance

--- src/com/bootcamp/probability/Chance.java
+++ src/com/bootcamp/probability/Chance.java
@@ -1,5 +1,7 @@
 package com.bootcamp.probability;
 
+import java.util.Objects;
+
 public class Chance {
 
     private final int chance;
@@ -15,5 +17,17 @@ public class Chance {
 
     public Chance compliment () {
         return new Chance(100 - chance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Chance chance1 = (Chance) o;
+        return chance == chance1.chance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(chance);
     }
 }
--- test/com/bootcamp/probability/ChanceTest.java
+++ test/com/bootcamp/probability/ChanceTest.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChanceTest {
-    @Test
-    void shouldGiveChanceOf50() {
-        Chance chance = new Chance(50);
-        assertEquals("50%", chance.toString());
-    }
 
     @Test
     void shouldGiveRemainingChance() {
         Chance chance = new Chance(70);
-        assertEquals("30%", chance.compliment().toString());
+        Chance expected = new Chance(30);
+
+        assertEquals(expected, chance.compliment());
     }
 }
